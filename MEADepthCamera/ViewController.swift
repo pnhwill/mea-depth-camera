91a41d4392877e224730ff86636c448e77c1058e
pnhwill <wharrington2016@my.fit.edu>
2021-07-13 11:48:50 -0400

    Initial Commit

---
+++ MEADepthCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MEADepthCamera
+//
+//  Created by Will on 7/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
